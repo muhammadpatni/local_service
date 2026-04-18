cbe306bf170235d3de1a7e1c2a84263c4c926998
haram <zainabharam137@gmail.com>
2026-04-18 10:37:54 +0500

    changes

--- lib/core/constants/app_strings.dart
+++
@@ -1,5 +0,0 @@
-class AppStrings {
-  static const login = "Login";
-  static const phoneLogin = "Login with Phone";
-  static const googleLogin = "Login with Google";
-}
--- lib/core/utils/snackbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppSnackbar {
-  static show(BuildContext context, String message) {
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(SnackBar(content: Text(message)));
-  }
-}
