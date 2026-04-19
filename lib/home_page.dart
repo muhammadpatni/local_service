39eb3d6e447e884095327c793e2c0fa88eb5a799
haram <zainabharam137@gmail.com>
2026-04-19 09:30:48 +0500

    refactor: Implement UserDrawer for navigation and update MyHomePage layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_service/user_drawer.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,6 +11,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Welcome to the Home Page!")));
+    return Scaffold(
+      drawer: UserDrawer(), // Aapka Drawer widget yahan use hona chahiye
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Center(child: Text("Welcome to the Home Page!")),
+    );
   }
 }
